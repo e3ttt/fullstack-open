8704fca4e63dff2a88cae6b6427db723a41bcdef
Edvan Trindade <trindade3e@gmail.com>
2022-08-27 09:05:36 -0300

    Finsihed exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 
 const Statistics = ({ good, neutral, bad }) => {
+  if (good == 0 && neutral == 0 && bad == 0) {
+    return <p>No feedback given</p>;
+  }
   return (
     <>
       <h1>statistics</h1>
